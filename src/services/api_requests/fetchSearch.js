3a794d5286d0c31d4ce49655417d8247afade110
sharmistha071 <shormy.roy@gmail.com>
2023-11-22 14:05:03 +0000

    API query services

---
+++ src/services/api_requests/fetchSearch.js
@@ -0,0 +1,14 @@
+async function fetchSearch({ queryKey }) {
+  console.log('fetchSearch', queryKey)
+  const { animal, location, breed } = queryKey[1]
+
+  const response = await fetch(
+    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+  )
+  if (!response.ok) {
+    throw new Error(`pet search not okay`)
+  }
+  return response.json()
+}
+
+export default fetchSearch
